d167634b97a28cb4238fcf691cc35a8de3b41567
Akshay Gautam <akshaygautam010@gmail.com>
2026-05-06 10:50:39 +0530

    feat: implement LlamaCppRuntimeManager for model lifecycle management and integrate into API server and routing

--- src/nexus_backend/models/__init__.py
+++ src/nexus_backend/models/__init__.py
@@ -4,10 +4,16 @@ from nexus_backend.models.registry import ModelArtifact
 from nexus_backend.models.registry import ModelRegistry
 from nexus_backend.models.registry import ModelRoleDefaults
 from nexus_backend.models.registry import discover_model_registry
+from nexus_backend.models.runtime import LlamaCppRuntimeManager
+from nexus_backend.models.runtime import RuntimeSettings
+from nexus_backend.models.runtime import RuntimeState
 
 __all__ = [
+    "LlamaCppRuntimeManager",
     "ModelArtifact",
     "ModelRegistry",
     "ModelRoleDefaults",
+    "RuntimeSettings",
+    "RuntimeState",
     "discover_model_registry",
 ]
